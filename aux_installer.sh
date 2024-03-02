1f06eefe88b342a3af04e2e8860d960b6d84fbfa
sergiogon <sergiogon@microsoft.com>
2024-03-02 15:33:30 +0100

    modified

--- aux_installer.sh
+++ aux_installer.sh
@@ -3,9 +3,8 @@ sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y 
 sudo apt-get install python-is-python3 -y 
 sudo apt-get install ffmpeg -y
-sudo apt-get install cmake -y
 
-pip install -r requirements.txt
+pip install -r aux_requirements.txt
 
 source config.sh
 mkdir $media_folder
