9d6f460f22fbca7e6f439a06c801c99d44a97ea2
sergiogon <sergiogon@microsoft.com>
2024-03-02 15:44:39 +0100

    bug

--- aux_installer.sh
+++ aux_installer.sh
@@ -4,6 +4,11 @@ sudo apt-get install python3-pip -y
 sudo apt-get install python-is-python3 -y 
 sudo apt-get install ffmpeg -y
 
+sudo apt-get install gstreamer1.0-plugins-bad -y 
+sudo apt-get install gstreamer1.0-plugins-base -y 
+sudo apt-get install gstreamer1.0-plugins-good -y 
+sudo apt-get install gstreamer1.0-plugins-ugly -y 
+
 pip install -r aux_requirements.txt
 
 source config.sh
